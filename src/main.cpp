5fd17a81244d4787697ff25f0da0bafe71684a42
Kai Wolf <kai.wolf@gmail.com>
2014-01-28 22:27:37 +0100

    Implementing pyramidal depth map computation

--- src/main.cpp
+++ src/main.cpp
@@ -145,7 +145,7 @@ cv::Mat lightPattern(int width, int height, int j, int N) {
     return img;
 }
 
-cv::Mat computeNormals(std::vector<cv::Mat> camImages) {
+cv::Mat computeNormals(std::vector<cv::Mat> camImages, cv::Mat Mask = cv::Mat()) {
     
     int height = camImages[0].rows;
     int width = camImages[0].cols;
@@ -180,7 +180,12 @@ cv::Mat computeNormals(std::vector<cv::Mat> camImages) {
             float sx = 128.0f + 127.0f * sgn(EV.at<float>(idx, 1)) * fabs(EV.at<float>(idx, 1)) * rSxyz;
             float sy = 128.0f + 127.0f * sgn(EV.at<float>(idx, 2)) * fabs(EV.at<float>(idx, 2)) * rSxyz;
             
-			N.at<cv::Vec3b>(i, j) = cv::Vec3b(sx, sy, sz);   
+            if (Mask.at<uchar>(cv::Point(j, i)) == 0) {
+                N.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 255);
+            } else {
+                N.at<cv::Vec3b>(i, j) = cv::Vec3b(sx, sy, sz);
+            }
+			  
             idx += 1;
         }
     }
@@ -188,44 +193,72 @@ cv::Mat computeNormals(std::vector<cv::Mat> camImages) {
     return N;
 }
 
-cv::Mat localHeightfield(cv::Mat Normals, cv::Mat Mask) {
-
-	int height = Normals.rows;
-    int width = Normals.cols;
-	cv::Mat Z(height, width, CV_32FC1, cv::Scalar::all(0));
-
-	for (int k = 0; k < 3000; k++) {
-		for (int i = 1; i < height-1; i++) {
-			for (int j = 1; j < width-1; j++) {
-				int up = Mask.at<uchar>(cv::Point(j,i-1));
-				int down = Mask.at<uchar>(cv::Point(j,i+1));
-				int left = Mask.at<uchar>(cv::Point(j-1,i));
-				int right = Mask.at<uchar>(cv::Point(j+1,i));
-				float zU = Z.at<float>(cv::Point(j,i-1));
+void updateHeights(cv::Mat &Normals, cv::Mat &Z, int iterations) {
+    
+    for (int k = 0; k < iterations; k++) {
+        for (int i = 1; i < Normals.rows-1; i++) {
+            for (int j = 1; j < Normals.cols-1; j++) {
+                float zU = Z.at<float>(cv::Point(j,i-1));
 				float zD = Z.at<float>(cv::Point(j,i+1));
 				float zL = Z.at<float>(cv::Point(j-1,i));
 				float zR = Z.at<float>(cv::Point(j+1,i));
 				float nxC = Normals.at<cv::Vec3b>(cv::Point(j,i))[0];
 				float nyC = Normals.at<cv::Vec3b>(cv::Point(j,i))[1];
 				float nxU = Normals.at<cv::Vec3b>(cv::Point(j,i-1))[0];
+                float nyU = Normals.at<cv::Vec3b>(cv::Point(j,i-1))[1];
+                float nxD = Normals.at<cv::Vec3b>(cv::Point(j,i+1))[0];
+                float nyD = Normals.at<cv::Vec3b>(cv::Point(j,i+1))[1];
+                float nxL = Normals.at<cv::Vec3b>(cv::Point(j-1,i))[0];
 				float nyL = Normals.at<cv::Vec3b>(cv::Point(j-1,i))[1];
-				if (up > 0 && down > 0 && left > 0 && right > 0) 
+                float nxR = Normals.at<cv::Vec3b>(cv::Point(j+1,i))[0];
+                float nyR = Normals.at<cv::Vec3b>(cv::Point(j+1,i))[1];
+                int up = nxU == 0 && nyU == 0 ? 0 : 1;
+                int down = nxD == 0 && nyD == 0 ? 0 : 1;
+                int left = nxL == 0 && nyL == 0 ? 0 : 1;
+                int right = nxR == 0 && nyR == 0 ? 0 : 1;
+				if (up > 0 && down > 0 && left > 0 && right > 0)
 					Z.at<float>(cv::Point(j,i)) = 1.0f/4.0f * ( zD + zU + zR + zL + nxU - nxC + nyL - nyC );
-				
-			}
-		}
-	}
+            }
+        }
+    }
+}
 
-	double min, max;
-	cv::minMaxIdx(Z, &min, &max);
+cv::Mat cvtFloatToGrayscale(cv::Mat F, int limit = 255) {
+    double min, max;
+	cv::minMaxIdx(F, &min, &max);
 	cv::Mat adjMap;
-	cv::convertScaleAbs(Z, adjMap, 255 / max);
-	cv::imshow("Local Depthmap", adjMap);
+	cv::convertScaleAbs(F, adjMap, limit / max);
+    return adjMap;
+}
+
+cv::Mat localHeightfield(cv::Mat Normals) {
+
+    const int pyramidLevels = 4;
+    const int iterations = 300;
+    
+    /* building image pyramid */
+    std::vector<cv::Mat> pyrNormals;
+    cv::Mat Normalmap = Normals.clone();
+    pyrNormals.push_back(Normalmap);
+    for (int i = 0; i < pyramidLevels; i++) {
+        cv::pyrDown(Normalmap, Normalmap);
+        pyrNormals.push_back(Normalmap.clone());
+    }
+    
+    /* updating depth map along pyramid levels, starting with smallest level at top */
+    cv::Mat Z(pyrNormals[pyramidLevels-1].rows, pyrNormals[pyramidLevels-1].cols, CV_32FC1, cv::Scalar::all(0));
+    for (int i = pyramidLevels-1; i > 0; i--) {
+        updateHeights(pyrNormals[i], Z, iterations);
+        cv::pyrUp(Z, Z);
+    }
+	cv::imshow("Local Depthmap", cvtFloatToGrayscale(Z));
 
     /* linear transformation of matrix values from [min,max] -> [a,b] */
+    double min, max;
+	cv::minMaxIdx(Z, &min, &max);
     double a = 0.0, b = 150.0;
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < Normals.rows; i++) {
+        for (int j = 0; j < Normals.cols; j++) {
             Z.at<float>(cv::Point(j,i)) = (float) a + (b-a) * ((Z.at<float>(cv::Point(j,i)) - min) / (max-min));
         }
     }
@@ -285,13 +318,17 @@ int main(int argc, char *argv[]) {
         cv::imshow(s.str(), camImages[i]);
     }
     
-    cv::Mat S = computeNormals(camImages);
+    /* threshold images */
+    cv::Mat Mask = imageMask(camImages);
+    
+    /* compute normal map */
+    cv::Mat S = computeNormals(camImages, Mask);
 	cv::Mat Normalmap;
-    S.copyTo(Normalmap, imageMask(camImages));
-	cv::cvtColor(Normalmap, Normalmap, CV_BGR2RGB);
+	cv::cvtColor(S, Normalmap, CV_BGR2RGB);
 	cv::imshow("normalmap.png", Normalmap);
 
-	cv::Mat Depth = localHeightfield(S, imageMask(camImages));
+    /* compute depth map */
+	cv::Mat Depth = localHeightfield(S);
     double min, max;
     cv::minMaxIdx(Depth, &min, &max);
 	displayMesh(Depth, camImages[0]);
